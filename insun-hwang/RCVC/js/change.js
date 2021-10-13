f2e3526affd319771a53ce3dd33b4ae33e142dd2
INSUNHWANG <spsun89@gmail.com>
2021-10-13 11:06:56 +0900

    update index.html

--- insun-hwang/RCVC/js/change.js
+++ insun-hwang/RCVC/js/change.js
@@ -3,7 +3,15 @@
 
 function changeImage(picture_id, path) {
     // alert("images/plot/" + path + ".PNG");
-    document.getElementById(picture_id).src = "images/plot/" + path + ".PNG"
+    // document.getElementById(picture_id).src = "images/plot/" + path + ".PNG"
+    // alert("wav/melspec/" + path + ".PNG");
+    // alert("wav/melsepc/" + path + ".PNG");
+    // document.getElementById(picture_id).src = 'wav/melsepc/' + path + '.PNG'
+    document.getElementById(picture_id).src = path
+
+    // alert("wav/melsepc/" + path + ".PNG"); # 'wav/melsepc/p269_p269_001_to_p256_p256_001.PNG'
+    // document.getElementById(picture_id).src = 'wav/melspec/p269_p269_001_to_p256_p256_001.PNG'
+    
 }
 
 
